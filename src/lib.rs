cfc6639073dc4d81008827227177d95624800001
Moritz Riede <moritz@mriede.com>
2024-01-14 09:43:23 +0100

    feat: restructure protocol

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,43 @@
+use serde::{Serialize, Deserialize};
+use chrono::DateTime;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub struct Packet {
+    pub timestamp: DateTime<chrono::Utc>,
+    pub message: Message,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+/// The possible interactions with client devices or other game controllers.
 pub enum Message {
+    Request(Request),
+    Response(Response),
+    Notice(Notice),
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub enum Response {
+    /// Sent by the master node to a client or controller to indicate that 
+    /// the connection was successful.
+    Connected {
+        id: u16,
+    },
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub enum Request {
     /// Initiate a WebSocket session with the master node.
-    Hello,
+    Hello {
+        /// Request to join the mesh as a GUI client.
+        is_client: bool,
+    },
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug)]
+pub enum Notice {
     /// Terminate the socket session.
     Farewell,
     /// Synchronize controller time.
